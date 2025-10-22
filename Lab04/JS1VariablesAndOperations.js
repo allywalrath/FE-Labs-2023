f4d56a9665f7634e40e1a0545ba237932d37eb9b
Alicia Walrath <walrath.alica@gmail.com>
2025-10-22 15:40:14 -0400

    finished the JS lab

--- Lab04/JS1VariablesAndOperations.js
+++ Lab04/JS1VariablesAndOperations.js
@@ -52,21 +52,33 @@ Question 1. Creating & 'console.log()'ing variables \n`);
  * ↓ YOUR CODE HERE ↓ */
 
 console.log("Step 1:  Number of Seats on a Plane");
+var availableFlightSeats = 7; //make variable names as clear as possible
+console.log("The number of seats left on this plane are " + availableFlightSeats) /*added a string to explain the variable, to be a little more specific - helps with debugging*/
 
 
 console.log("Step 2:  Cost of Groceries");
+var totalGroceryCost = 100.53;
+console.log("The cost of groceries is " + totalGroceryCost)
 
 
 console.log("Step 3:  Middle Initial");
+var middleInitial = "N";
+console.log("My middle initial is" + " " + middleInitial)
 
 
 console.log("Step 4:  Is it Hot Outside?");
+var isItHotOutside = false;
+console.log("Is it hot outside?" + " " + isItHotOutside)
 
 
 console.log("Step 5:  First Name");
+var firstName = "Alicia";
+console.log("The variable firstName is" + " " + firstName)
 
 
 console.log("Step 6:  Street Address");
+var streetAddress = "100 Edgar's Lane, Hastings on Hudson NY, 10706;"
+console.log("A real address in Southern New York State is" + " " + streetAddress)
 
 
 /*-------------------------------------------------------*/
@@ -85,33 +97,45 @@ Question 2. Creating & 'console.log()'ing variables \n`);
     console.log('Remove Plane Seats:')
 //	A customer booked 2 plane seats. Remove 2 seats from the available seats variable
 //  ↓ YOUR CODE HERE ↓
+console.log(availableFlightSeats - 2)
+console.log("A customer booked 2 seats. Now there are only" + " " + (availableFlightSeats - 2) + " " + "seats left.")
 
 
     console.log('Buy a Candy Bar:')
 //	Impulse candy bar purchase! Add 2.15 to the grocery total
 //  ↓ YOUR CODE HERE ↓
+console.log(totalGroceryCost + 2.15)
+console.log("I bought a candy bar. Now my total grocery cost is" + " " + (totalGroceryCost + 2.15))
 
 
     console.log('Fix a Birth Certificate:')
 //	A birth certificate was printed incorrectly, change the middle initial to something else
 //  ↓ YOUR CODE HERE ↓
+middleInitial = "Z"
+console.log("Middle initial was actually" + " " + middleInitial)
 
 
     console.log('Changing Seasons:')
 //	The season has changed, update the hot outside variable	to be opposite of what it was
 //  ↓ YOUR CODE HERE ↓
+console.log(!isItHotOutside) //the exclamation point makes something the opposite of itself, so in this case it made it "true" - it's called a "bing" operator or a "not" operator
+isItHotOutside = true
+console.log(isItHotOutside)
     
 
     console.log('Full Name:')
 //	Create a new variable called full name using the customer's
 //  first name, the middle initial, and a last name of your choice
 //  ↓ YOUR CODE HERE ↓
-
+var lastName = "Walrath";
+var fullName = firstName + " " + middleInitial + " " + lastName;
+console.log("My full name is" + " " + fullName)
 
     console.log('Introduce a Customer:')
 //	Print a line to the console that introduces the customer 
 //	including the name, and saying that they live at the address variable
 //  ↓ YOUR CODE HERE ↓
+console.log("Here is a new customer. Her name is " + fullName + ". " + "She lives at " + streetAddress)
 
 
 
