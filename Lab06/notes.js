a5696915c468056c116e3d0cce55d92ab69d65fa
Alicia Walrath <walrath.alica@gmail.com>
2026-02-23 21:13:06 -0500

    adding the rest of the lab06 files to the repo - no longer working from the test folder for lab 6

---
+++ Lab06/notes.js
@@ -0,0 +1,27 @@
+/*console.logging array.pop() will just show what got removed from the array, not the array. to see the array you 
+console.log the array's name*/
+
+//HARD QUESTIONS:
+// eight, seven, six, 
+
+// 5 was slightly difficult but I don't think it qualifies as hard, harder than nine though
+// 4 was slightly harder than 5
+// 3 was somewhere in that mix
+
+
+//array1[array1.length - 1]);
+
+// 1 and 2 were almost just as easy as each other, lumping them together
+
+// 9 was the easiest lol
+
+/*
+8
+7
+6
+4
+5
+3
+2
+1
+*/
